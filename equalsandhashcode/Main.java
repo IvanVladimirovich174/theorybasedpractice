6ebce57a5a855426eca7af9f83150b0d682280c8
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-11-18 02:34:20 +0300

    equalsandhashcode

---
+++ equalsandhashcode/Main.java
@@ -0,0 +1,34 @@
+package equalsandhashcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Ivan174", "Ivan", "Chelyabinsk");
+        User user2 = new User("Ivan174", "Ivan", "Chelyabinsk");
+        User user3 = new User("JamesGosling", "James", "Calgary");
+
+        // True
+        boolean equalsValue = user1.equals(user2);
+        System.out.println("EqualsValue = " + equalsValue);
+        System.out.println("User1 hashCode = " + user1.hashCode());
+        System.out.println("User2 hashCode = " + user2.hashCode());
+        System.out.println();
+
+        // False
+        equalsValue = user1.equals(user3);
+        System.out.println("EqualsValue = " + equalsValue);
+        System.out.println("User1 hashCode = " + user1.hashCode());
+        System.out.println("User3 hashCode = " + user3.hashCode());
+        System.out.println();
+
+        // Contains method use equals method
+        List<User> listUsers = new ArrayList<>();
+        listUsers.add(user1);
+        listUsers.add(user2);
+
+        System.out.println("Is ListUsers contain user3 ?");
+        System.out.println(listUsers.contains(user3));
+    }
+}
---
+++ equalsandhashcode/User.java
@@ -0,0 +1,60 @@
+package equalsandhashcode;
+
+import java.util.Objects;
+
+public class User {
+    private String login;
+    private String name;
+    private String city;
+
+    public User(String login, String name, String city) {
+        this.login = login;
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (this == obj) {
+            return false;
+        }
+
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        User other = (User) obj;
+
+        return (this.login.equals(other.login)) && (this.name.equals(other.name)) && (this.city.equals(other.city));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.login, this.name, this.city);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
