ef79b11e325a07f0b05b111e0c7099e6f8cc09df
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-11-22 21:52:42 +0300

    comparableandcomparator

--- comparableandcomparator/Main.java
+++ comparableandcomparator/Main.java
@@ -1,19 +1,38 @@
 package comparableandcomparator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
 
-        users.add(new User("Ivan",  31,"MoScow"));
-        users.add(new User("Sergey",  29,"mOScow"));
+        // CompareTo
+        users.add(new User("Ivan", 31, "MoScow"));
+        users.add(new User("Sergey", 29, "mOScow"));
         users.add(new User("James", 27, "Calgary"));
         System.out.println("list = " + users);
 
         users.sort(User::compareTo);
 
+        System.out.println("List after sort = " + users);
+
+        // Clear
+        users.clear();
+
+        // Comparator
+        users.add(new User("Ivan", 31, "Amsterdam"));
+        users.add(new User("Sergey", 29, "Moscow"));
+        users.add(new User("James", 27, "Calgary"));
+        System.out.println("list = " + users);
+
+        Comparator<User> cmpByAge = new UserByAgeComparator();
+        users.sort(cmpByAge);
+        System.out.println("List after sort = " + users);
+
+        Comparator<User> cmpByCity = new UserByCityComparator();
+        users.sort(cmpByCity);
         System.out.println("List after sort = " + users);
     }
 }
---
+++ comparableandcomparator/UserByCityComparator.java
@@ -0,0 +1,10 @@
+package comparableandcomparator;
+
+import java.util.Comparator;
+
+public class UserByCityComparator implements Comparator<User> {
+    @Override
+    public int compare(User o1, User o2) {
+        return o1.getCity().compareToIgnoreCase(o2.getCity());
+    }
+}
