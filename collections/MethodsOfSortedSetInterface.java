023776e9ce3ec96582bd19f412ddd29d4c985378
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-11-23 23:29:38 +0300

    collections

---
+++ collections/MethodsOfSortedSetInterface.java
@@ -0,0 +1,47 @@
+package collections;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class MethodsOfSortedSetInterface {
+    public static void main(String[] args) {
+        SortedSet<Integer> treeSet = new TreeSet<>();
+
+        // Automatically creates ascending (lexicographic) order
+        for (int i = 10; i >= 1; i--) {
+            treeSet.add(i);
+        }
+
+        System.out.println("Result:");
+        for (Integer number : treeSet) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        // .first() and .last() methods
+        Integer firstValue = treeSet.first();
+        System.out.println("First value = " + firstValue);
+
+        Integer lastValue = treeSet.last();
+        System.out.println("Last value = " + lastValue);
+
+        // .subSet() method
+        SortedSet<Integer> subSet = treeSet.subSet(2, 10);
+        System.out.println("SubSet: " + subSet);
+
+        subSet = treeSet.subSet(2, 200);
+        System.out.println("SubSet: " + subSet);
+
+        // .headSet() method
+        SortedSet<Integer> headSet = treeSet.headSet(5);
+        System.out.println("HeadSet: " + headSet);
+
+
+        headSet = treeSet.headSet(-10);
+        System.out.println("HeadSet: " + headSet);
+
+        // .tailSet() method
+        SortedSet<Integer> tailSet = treeSet.tailSet(7);
+        System.out.println("TailSet: " + tailSet);
+    }
+}
