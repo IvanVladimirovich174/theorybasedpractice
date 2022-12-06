de122a07d03e09f9b7ed688fce0180a7b9c5f448
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-12-06 13:48:59 +0300

    reflection

--- reflection/getconstructorsandmethods/GetConstructorsAndMethods.java
+++ reflection/getconstructorsandmethods/GetConstructorsAndMethods.java
@@ -11,6 +11,8 @@ public class GetConstructorsAndMethods {
         Class<Test> cls = Test.class;
 
         // .getDeclaredConstructor() method
+        System.out.println("Get declared constructor:");
+
         Constructor<Test> constructor;
 
         try {
@@ -21,39 +23,69 @@ public class GetConstructorsAndMethods {
             throw new RuntimeException(e);
         }
 
+        System.out.println();
+
         // .newInstance()
+        System.out.println("Create instance of class with help of reflection:");
+
         Test test;
+
         try {
             test = constructor.newInstance(123, "Test string");
             System.out.println(test);
+
+            System.out.println("Values of fields:");
             System.out.println(test.a);
             System.out.println(test.b);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
 
+        System.out.println();
+
+        // get parameters of constructor
+        System.out.println("Get parameters of constructor:");
+
+        for (Constructor<?> clsIter : cls.getConstructors()) {
+            Class<?>[] parametersOfConstructor = clsIter.getParameterTypes();
+            for (Class<?> parameter : parametersOfConstructor) {
+                System.out.println(parameter.getName());
+            }
+        }
+
+        System.out.println();
+
         // .getMethod()
         try {
             Method method = cls.getMethod("printHelloWorld");
-            System.out.println("Get method name: " + method.getName());
+            System.out.println("Get method by name: " + method.getName());
 
             // invoke .printHelloWorld() method
+            System.out.println("Invoke .printHelloWorld() method:");
             method.invoke(test);
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
+
         System.out.println();
 
         // print all public methods of class and superclasses
-        System.out.println("Print all public methods");
+        System.out.println("Print all public methods of class " +
+                "and superclasses");
+
         Arrays.stream(cls.getMethods()).forEach(i -> {
             System.out.println("Method name " + i.getName() + " " + i.getDeclaringClass().getName());
         });
 
+        System.out.println();
+
         // print all methods of class
-        System.out.println("Print ");
+        System.out.println("Print all methods of class");
+
         Arrays.stream(cls.getDeclaredMethods()).forEach(i -> {
             System.out.println("Method name " + i.getName() + " " + i.getDeclaringClass().getName());
         });
+
+        System.out.println();
     }
 }
