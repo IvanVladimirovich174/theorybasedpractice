baa30c7ca9d6e0fa9d197ee40afd12dbb63bc42d
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-12-28 16:31:50 +0300

    strings

--- strings/StringPool.java
+++ strings/StringPool.java
@@ -9,10 +9,18 @@ public class StringPool {
         // Rest part of heap
         String stringViaConstructor = new String("Java");
 
+        // StringPool
         String str1 = "test";
         String str2 = "test";
 
         System.out.println("Is str1 == str2? " + (str1 == str2));
+        System.out.println();
+
+        // Rest
+        String str3 = "TopJava";
+        String str4 = "Top" + "Java";
+
+        System.out.println("Is str3 == str4 " + (str3 == str4));
         System.out.println();
     }
 }
