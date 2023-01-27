78e85d164c197b7e31bdc34eda5c8fe9b571768a
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-01-27 10:55:08 +0500

    streams

--- streams/StreamsTest.java
+++ streams/StreamsTest.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamsTest {
@@ -25,11 +29,33 @@ public class StreamsTest {
                 .forEach(System.out::print);
         System.out.println();
 
-        List<String> list = Stream.of(1, 2, 3, 4, 5)
+        List<String> listOfStrings = Stream.of(1, 2, 3, 4, 5)
                 .filter((Integer digit) -> digit > 2)
                 .map((Integer digit) -> "str_" + digit)
                 .toList();
+        listOfStrings.forEach(System.out::println);
 
-        list.forEach(System.out::println);
+        // ignore result os stream
+        List<Integer> listOfIntegers = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            listOfIntegers.add(i);
+        }
+
+        System.out.println(listOfIntegers);
+
+        List<Integer> anotherList = listOfIntegers.stream()
+                .filter((Integer digit) -> (digit >= 2 && digit <= 4))
+                .map((Integer digit) -> digit * 2)
+                .collect(Collectors.toList());
+        System.out.println(listOfIntegers);
+        System.out.println(anotherList);
+
+
+        Map<Integer,Integer> set = listOfIntegers.stream()
+                .filter((Integer digit) -> (digit >= 2 && digit <= 4))
+                .map((Integer digit) -> digit * 2)
+                .collect(Collectors.toMap(k -> k, a -> 2 * a));
+        System.out.println(set);
     }
 }
