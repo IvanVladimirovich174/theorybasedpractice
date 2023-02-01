3401436bfbb1520b980cb7ae7444218c4585f666
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-02-01 23:53:25 +0500

    springxmlconfiguration

--- springxmlconfiguration/framework/MainFramework.java
+++ springxmlconfiguration/framework/MainFramework.java
@@ -1,5 +1,28 @@
 package framework;
 
-public class MainFramework {
+import messageprovider.MessageProvider;
+import messageprovider.MessageRenderer;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MainFramework {
+    private final MessageProvider messageProvider;
+    private final MessageRenderer messageRenderer;
+    public MainFramework() {
+        Properties properties = new Properties();
+
+        try (FileInputStream fileInputStream = new FileInputStream("config.properties")) {
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String renderedClass = properties.getProperty("render.class");
+        String messageProviderClass = properties.getProperty("provider.class");
+
+        messageProvider = Class.forName(messageProviderClass);
+        messageRenderer = Class.forName(renderedClass);
+    }
 }
--- springxmlconfiguration/messageprovider/Main.java
+++ springxmlconfiguration/messageprovider/Main.java
@@ -1,14 +1,9 @@
 package messageprovider;
 
-import messageprovider.DefaultMessageProvider;
-import messageprovider.JsonMessageRenderer;
+import framework.MainFramework;
 
 public class Main {
     public static void main(String[] args) {
-        MessageProvider messageProvider = new DefaultMessageProvider();
-        MessageRenderer renderer = new JsonMessageRenderer();
-        String message = messageProvider.getMessage();
-
-        renderer.render(message);
+        MainFramework mainFramework = new MainFramework();
     }
 }
