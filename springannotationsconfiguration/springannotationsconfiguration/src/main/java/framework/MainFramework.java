7a43765ca85d1866da0f04f2e8d7749d15ace3a4
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-02-06 22:48:31 +0500

    springxmlconfiguration

--- springannotationsconfiguration/springannotationsconfiguration/src/main/java/framework/MainFramework.java
+++ springannotationsconfiguration/springannotationsconfiguration/src/main/java/framework/MainFramework.java
@@ -1,4 +1,21 @@
 package framework;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class MainFramework {
+
+    public MainFramework() {
+        Properties properties = new Properties();
+
+        try (FileInputStream fileInputStream = new FileInputStream("config.properties")) {
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read properties error= " + e.toString(), e);
+        }
+
+        String renderClass = properties.getProperty("render.class");
+        String messageProviderClass = properties.getProperty("provider.class");
+    }
 }
