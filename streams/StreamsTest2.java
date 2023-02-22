cf6c3ea64d8bbf346c2e5424d258fbc6203a702a
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-02-22 21:11:48 +0500

    streams

--- streams/StreamsTest2.java
+++ streams/StreamsTest2.java
@@ -3,10 +3,18 @@ import java.util.stream.Stream;
 
 public class StreamsTest2 {
     public static void main(String[] args) {
-        List<String> myPlaces = List.of("Nepal", "Cairo", "Phuket", "New-York");
+        List<String> myPlaces = List.of("New-York", "Cairo", "Phuket", "Nepal");
         Stream<String> stringStream = myPlaces.stream();
+
         stringStream.forEach(i -> {
             System.out.println(i.toUpperCase());
         });
+        System.out.println();
+
+        myPlaces.stream()
+                .filter((s) -> s.startsWith("N"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
     }
 }
