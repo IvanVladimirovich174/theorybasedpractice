4725c6bba1df649d6b9da0ac15d89bf6426cc6cb
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-02-24 06:05:27 +0500

    squarenumber

--- functionalinterfaces/squarenumber/Main.java
+++ functionalinterfaces/squarenumber/Main.java
@@ -1,20 +1,10 @@
 package functionalinterfaces.squarenumber;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Square s = (int x) -> x * x;
 
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-
-        numbers.stream()
-                .map(s::calculate)
-                .forEach(System.out::println);
+        int result = s.calculate(5);
+        System.out.println(result);
     }
 }
