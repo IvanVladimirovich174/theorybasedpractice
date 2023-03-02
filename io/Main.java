b182a4ba20a73a0acf8485238bdfbf762c9a872f
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-03-02 23:43:26 +0500

    io

---
+++ io/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = System.in;
+        int asciiCode = inputStream.read();
+
+        System.out.println("ASCII Code=" + asciiCode + " (char representation=" + (char) asciiCode + ")");
+    }
+}
