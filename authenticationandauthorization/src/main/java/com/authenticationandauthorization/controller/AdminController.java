5f08ece36d207a23b98d6f6d048c2f27a7e194b8
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-03-09 03:11:06 +0500

    authenticationandauthorization

--- authenticationandauthorization/src/main/java/com/authenticationandauthorization/controller/AdminController.java
+++ authenticationandauthorization/src/main/java/com/authenticationandauthorization/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.authenticationandauthorization.controller;
 
+import com.authenticationandauthorization.service.Product;
 import com.authenticationandauthorization.service.ProductCache;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -22,7 +24,12 @@ public class AdminController {
     }
 
     @PostMapping(value = "/update")
-    public ModelAndView updateProduct() {
-        return new ModelAndView("/update_product.jsp");
+    public ModelAndView updateProduct(@RequestParam("id") String id,
+                                      @RequestParam("name") String name,
+                                      @RequestParam("price") String price) {
+        Product info = new Product(id, name, Double.parseDouble(price));
+        productCache.update(info);
+
+        return new ModelAndView("/success_create_product.jsp");
     }
 }
