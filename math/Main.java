9218a9afaaa8a8fee121c2023a7c27c4782849f4
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-03-25 22:24:24 +0500

    math

---
+++ math/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 0.553;
+
+        System.out.println(a);
+
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.round(a));
+        System.out.println(Math.floor(a));
+        System.out.println(Math.rint(a));
+    }
+}
