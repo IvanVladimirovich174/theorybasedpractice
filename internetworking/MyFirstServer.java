c1034051c4e28dbefe2b140c08a5c376e56bb18b
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-03-29 01:47:01 +0500

    internetworking

--- internetworking/MyFirstServer.java
+++ internetworking/MyFirstServer.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,9 +18,36 @@ public class MyFirstServer {
         while (true) {
             Socket socket = serverSocket.accept();
 
-            processConnetcion(socket);
+            try {
+                processConnetcion(socket);
+            } catch (Exception ex) {
+                System.out.println("Failed to .processConnection() error=" + ex.toString());
+                ex.printStackTrace();
+            } finally {
+                socket.close();
+            }
+
         }
     }
 
-    private void processConnetcion(Socket socket) {
+    private void processConnetcion(Socket socket) throws IOException {
+        System.out.println("Method .processConnection() started");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        char[] message = new char[512];
+
+        reader.read(message);
+
+        System.out.println("IN:");
+        System.out.println(message);
+        System.out.println();
+
+
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        writer.write("HTTP/1.1 200 OK");
+        writer.println();
+        writer.flush();
+
+        System.out.println("Method processConnection() finished");
     }
+}
