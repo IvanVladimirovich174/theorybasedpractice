3971d04992c7c44a37ee1d772d904a4449bfa682
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-03-30 17:20:26 +0500

    Main

--- math/Main.java
+++ math/Main.java
@@ -19,5 +19,8 @@ public class Main {
 
         int min = Math.min(1, 2);
         System.out.println(min);
+
+        double sin = Math.sin(Math.PI / 2);
+        System.out.println(sin);
     }
 }
