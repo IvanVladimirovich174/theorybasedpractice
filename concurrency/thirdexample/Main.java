a65eba4b1db04badb3213c5cf5301c9641513b9f
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-04-16 00:53:28 +0500

    concurrency

--- concurrency/thirdexample/Main.java
+++ concurrency/thirdexample/Main.java
@@ -4,10 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Counter counter = new Counter();
 
         List<Thread> threadList = new ArrayList<>();
+
+        for (int i = 0; i < 10; ++i) {
+            Runnable task = () -> {
+                System.out.println("Thread started id= " + Thread.currentThread().getId());
+
+                for (int j = 0; j < 1000; ++j) {
+                    counter.increment();
+                }
+
+                System.out.println("Thread finished id= " + Thread.currentThread().getId());
+            };
+
+            threadList.add(new Thread(task));
+        }
+
+        for (Thread thread : threadList) {
+            thread.start();
+        }
+
+        for (Thread thread : threadList) {
+            thread.join();
+        }
 
         System.out.println(counter);
     }
