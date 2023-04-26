ede92296cb3bc59b1eae0a7971844a97fc495086
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-04-26 18:55:20 +0500

    concurrency

--- concurrency/seventhexample/Main.java
+++ concurrency/seventhexample/Main.java
@@ -5,15 +5,27 @@ import java.util.concurrent.*;
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Callable<String> task = () -> {
-            System.out.println("In task thread=" + getThreadInfo());
+            System.out.println("Custom task started thread=" + getThreadInfo());
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Do something");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    break;
+                }
+
+            }
+            System.out.println("Custom task finished thread=" + getThreadInfo());
             return "Hello world!";
         };
 
         ExecutorService executorService = Executors.newFixedThreadPool(4);
         Future<String> future = executorService.submit(task);
-        String result = future.get();
+        Thread.sleep(5000);
 
-        System.out.println("Main thread finished" + getThreadInfo() + " result" + result);
+        future.cancel(true);
+
+        System.out.println("Main thread finished " + getThreadInfo());
     }
 
     private static String getThreadInfo() {
