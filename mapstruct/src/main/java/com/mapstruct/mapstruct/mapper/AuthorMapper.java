43a86a06c47b7fc79d4a05b1e7b86cf922f5ab01
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-05-13 02:24:08 +0500

    mapstruct

--- mapstruct/src/main/java/com/mapstruct/mapstruct/mapper/AuthorMapper.java
+++ mapstruct/src/main/java/com/mapstruct/mapstruct/mapper/AuthorMapper.java
@@ -2,13 +2,15 @@ package com.mapstruct.mapstruct.mapper;
 
 import com.mapstruct.mapstruct.dto.AuthorDto;
 import com.mapstruct.mapstruct.model.Author;
-import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
     AuthorDto toDto(Author author);
 
-    @InheritConfiguration
     Author toEntity(AuthorDto authorDto);
+
+    List<AuthorDto> toListBookDto(List<Author> author);
 }
--- mapstruct/src/main/java/com/mapstruct/mapstruct/mapper/BookMapper.java
+++ mapstruct/src/main/java/com/mapstruct/mapstruct/mapper/BookMapper.java
@@ -2,13 +2,15 @@ package com.mapstruct.mapstruct.mapper;
 
 import com.mapstruct.mapstruct.dto.BookDto;
 import com.mapstruct.mapstruct.model.Book;
-import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
     BookDto toDto(Book book);
 
-    @InheritConfiguration
     Book toEntity(BookDto bookDto);
+
+    List<BookDto> toListDto(List<Book> book);
 }
