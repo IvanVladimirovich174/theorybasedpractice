00134cececf8eff317ee7752a7651b84a68ebf25
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-05-15 01:17:52 +0500

    mapstruct

--- mapstruct/src/main/java/com/mapstruct/mapstruct/dto/BookWithAuthorsDto.java
+++ mapstruct/src/main/java/com/mapstruct/mapstruct/dto/BookWithAuthorsDto.java
@@ -1,17 +1,13 @@
 package com.mapstruct.mapstruct.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Set;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class BookWithAuthorsDto extends BookDto {
     @JsonProperty("authorsDto")
     Set<AuthorDto> authorsDto;
--- mapstruct/src/main/java/com/mapstruct/mapstruct/mapper/BookMapper.java
+++ mapstruct/src/main/java/com/mapstruct/mapstruct/mapper/BookMapper.java
@@ -3,7 +3,6 @@ package com.mapstruct.mapstruct.mapper;
 import com.mapstruct.mapstruct.dto.BookDto;
 import com.mapstruct.mapstruct.dto.BookWithAuthorsDto;
 import com.mapstruct.mapstruct.model.Book;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -19,7 +18,6 @@ public interface BookMapper {
 
 
     @Mapping(source = "authors", target = "authorsDto", qualifiedByName = "toAuthorDto")
-    @InheritInverseConfiguration
     BookWithAuthorsDto toBookWithAuthorsDto(Book book);
 
     List<BookDto> toListDto(List<Book> book);
