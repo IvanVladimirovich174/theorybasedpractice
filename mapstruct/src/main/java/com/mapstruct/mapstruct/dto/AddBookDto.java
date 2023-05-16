bf7b44afecfc9b8836e2a089b742db37cd791a5e
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-05-17 00:34:56 +0500

    mapstruct

--- mapstruct/src/main/java/com/mapstruct/mapstruct/dto/AddBookDto.java
+++ mapstruct/src/main/java/com/mapstruct/mapstruct/dto/AddBookDto.java
@@ -1,11 +1,15 @@
 package com.mapstruct.mapstruct.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AddBookDto {
+    @JsonProperty("authorId")
     private Long authorId;
+
+    @JsonProperty("bookId")
     private Long bookId;
 }
