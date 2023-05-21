08b02c60aa4e878e2c8146f7b3de8c41fb296e29
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-05-21 21:41:31 +0500

    strings

--- strings/MethodsOfStringBuilderClass.java
+++ strings/MethodsOfStringBuilderClass.java
@@ -4,5 +4,10 @@ public class MethodsOfStringBuilderClass {
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder("Ivan Meshkov");
         System.out.println(stringBuilder);
+
+        stringBuilder = new StringBuilder(12);
+        System.out.println(stringBuilder.isEmpty());
+
+
     }
 }
