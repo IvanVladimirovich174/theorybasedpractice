b7013f579a484f415597804ccffd17dad3623ebd
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-05-29 22:30:49 +0500

    MyLinkedList

--- ownlinkedlist/MyLinkedList.java
+++ ownlinkedlist/MyLinkedList.java
@@ -13,20 +13,27 @@ public class MyLinkedList {
     }
 
     public MyLinkedList() {
-        size = 0;
-        head = null;
+        this.size = 0;
+        this.head = null;
     }
 
     public void addAtHead(int value) {
         Node node = new Node(value, head);
-        head = node;
+
+        this.head = node;
         ++size;
     }
 
     public void addAtTail(int value) {
-        Node node = head;
-        for (int i = 0; i < size - 1; ++i) {
-            node = node.next;
+        if (this.size == 0) {
+            Node node = this.head;
+            for (int i = 0; i < size - 1; ++i) {
+                node = node.next;
+            }
+            node.next = new Node(value, null);
+        } else {
+            this.head = new Node(value, null);
         }
+        ++size;
     }
 }
