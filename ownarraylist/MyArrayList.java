244b3a6b84b80e2d5de1eabe45a772faf6405372
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-06-02 22:49:30 +0500

    ownarraylist

---
+++ ownarraylist/MyArrayList.java
@@ -0,0 +1,29 @@
+public class MyArrayList {
+    private static final int DEFAULT_CAPACITY = 5;
+    private int[] array;
+    private int size;
+    private int capacity;
+
+    public MyArrayList() {
+        this.capacity = DEFAULT_CAPACITY;
+        this.size = 0;
+        this.array = new int[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.array = new int[capacity];
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+}
