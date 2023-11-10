7dafaa18551158619465af93f76c78a50945eb0b
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-11-10 23:55:54 +0500

    firstexample

--- sql/firstexample/firstexample.sql
+++ sql/firstexample/firstexample.sql
@@ -1,4 +1,7 @@
-CREATE TABLE student (
-    id constraint primary key,
-    name varchar(25)
-)
+CREATE TABLE weather (
+                         city            varchar(80),
+                         temp_lo         int,           -- минимальная температура дня
+                         temp_hi         int,           -- максимальная температура дня
+                         prcp            real,          -- уровень осадков
+                         date            date
+);
