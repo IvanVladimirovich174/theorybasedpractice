4615526e80107c6f34d66822cd2e7b3e88f2b755
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-11-12 23:51:06 +0500

    firstexample

--- sql/firstexample/firstexample.sql
+++ sql/firstexample/firstexample.sql
@@ -1,6 +1,11 @@
 CREATE TABLE student (
-                         id              int primary key,
-                         name            char(25),      -- имя студента
-                         age             int,           -- возраст
-                         birthDate       date
+    id              int primary key,
+    name            char(25),      -- имя студента
+    age             int,           -- возраст студента
+    birthDate       date           -- дата рождения студента
+);
+
+CREATE TABLE student (
+   id              int primary key,
+   course_name     char(25)        -- название курса
 );
